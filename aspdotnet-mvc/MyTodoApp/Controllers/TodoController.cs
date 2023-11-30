45afb37f6c4e3856f47e572b1066b71fc77dea8e
gil-son <gilszon@gmail.com>
2023-11-30 14:16:17 -0300

    Organized

--- aspdotnet-mvc/MyTodoApp/Controllers/TodoController.cs
+++ aspdotnet-mvc/MyTodoApp/Controllers/TodoController.cs
@@ -9,23 +9,30 @@ using Microsoft.EntityFrameworkCore;
 using MyTodoApp.Data;
 using MyTodoApp.Models;
 
-namespace MyTodoApp
+namespace MyTodoApp.Controllers
 {
     [Authorize]
     public class TodoController : Controller
     {
         private readonly ApplicationDbContext _context;
 
-        public TodoController(ApplicationDbContext context)
+       private readonly ILogger<TodoController> _logger;
+
+        public TodoController(ApplicationDbContext context, ILogger<TodoController> logger)
         {
             _context = context;
+            _logger = logger;
         }
 
         // GET: Todo
         public async Task<IActionResult> Index()
         {
               return _context.Todos != null ? 
-                          View(await _context.Todos.ToListAsync()) :
+                          View(await _context.Todos
+                          .AsNoTracking()
+                          //Just the logged user can view yourself tasks
+                          .Where( x => x.User == User.Identity.Name)
+                          .ToListAsync()) :
                           Problem("Entity set 'ApplicationDbContext.Todos'  is null.");
         }
 
@@ -40,6 +47,12 @@ namespace MyTodoApp
             var todo = await _context.Todos
                 .FirstOrDefaultAsync(m => m.Id == id);
             if (todo == null)
+            {
+                return NotFound();
+            }
+
+            // Take the logged user. So only the logged user can manage yourself task
+            if (todo.User != User.Identity.Name)
             {
                 return NotFound();
             }
@@ -60,12 +73,29 @@ namespace MyTodoApp
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Create([Bind("Id,Title,Done,CreatedAt,LastUpdateDate,User")] Todo todo)
         {
+            // Take the logged user to attribute the name
+            todo.User = User.Identity.Name;
+
             if (ModelState.IsValid)
             {
+
                 _context.Add(todo);
                 await _context.SaveChangesAsync();
+
+                _logger.LogInformation("Redirect to Index");
+
                 return RedirectToAction(nameof(Index));
+            
             }
+            else
+            {
+                foreach (var error in ModelState.Values.SelectMany(v => v.Errors))
+                {
+                    _logger.LogError($"Model error: {error.ErrorMessage}");
+                }
+            }
+
+
             return View(todo);
         }
 
@@ -82,6 +112,13 @@ namespace MyTodoApp
             {
                 return NotFound();
             }
+
+            // Take the logged user. So only the logged user can manage yourself task
+            if (todo.User != User.Identity.Name)
+            {
+                return NotFound();
+            }
+
             return View(todo);
         }
 
@@ -90,17 +127,22 @@ namespace MyTodoApp
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("Id,Title,Done,CreatedAt,LastUpdateDate,User")] Todo todo)
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Title,Done,User")] Todo todo)
         {
             if (id != todo.Id)
             {
                 return NotFound();
             }
 
+
             if (ModelState.IsValid)
             {
                 try
                 {
+                    todo.User = User.Identity.Name;
+                    todo.LastUpdateDate = DateTime.Now;
+                    
+
                     _context.Update(todo);
                     await _context.SaveChangesAsync();
                 }
@@ -117,6 +159,13 @@ namespace MyTodoApp
                 }
                 return RedirectToAction(nameof(Index));
             }
+            else
+            {
+                foreach (var error in ModelState.Values.SelectMany(v => v.Errors))
+                {
+                    _logger.LogError($"Model error: {error.ErrorMessage}");
+                }
+            }
             return View(todo);
         }
 
@@ -131,6 +180,12 @@ namespace MyTodoApp
             var todo = await _context.Todos
                 .FirstOrDefaultAsync(m => m.Id == id);
             if (todo == null)
+            {
+                return NotFound();
+            }
+
+            // Take the logged user. So only the logged user can manage yourself task
+            if (todo.User != User.Identity.Name)
             {
                 return NotFound();
             }
